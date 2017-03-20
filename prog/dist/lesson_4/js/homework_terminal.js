512d7f5e65162767cc87dcc7de841f2a67a73c60
Olexandr Kononchuk <okononchuk@grossum.com>
2017-03-20 18:07:19 +0200

    frontend pro lesson 4 homework started

---
+++ prog/dist/lesson_4/js/homework_terminal.js
@@ -0,0 +1,6 @@
+/**
+ * Created by shura on 20.03.17.
+ */
+let terminal = {
+
+};
--- prog/frontend_pro/lesson_3/js/functions_home.js
+++ prog/frontend_pro/lesson_3/js/functions_home.js
@@ -31,23 +31,56 @@ document.write("<h3>Exercise 2</h3>");
 
 function someNumber(a) {
     (a > 0) ? document.write("<p>Number is positive</p>") : document.write("<p>Number is not positive</p>");
+    var text = "<p>Number is not simple</p>";
     for (var i = 2; i < Math.abs(a); i++) {
-        (a % i === 0) ? document.write("<p>Number is not simple</p>") : document.write("<p>Number is not simple</p>");
+        if (a % i === 0) {
+            text = "<p>Number is not simple</p>";
+            break;
+        }
     }
+    document.write(text);
     var res;
-    (res = a % 2 === 0) ?
-        document.write("<p>" + a + " % 2 is not complete </p>") :
-        document.write("<p>" + a + " % 2 = " + res + "</p>");
-    (res = a % 3 === 0) ?
-        document.write("<p>" + a + " % 3 is not complete </p>") :
-        document.write("<p>" + a + " % 3 = " + res + "</p>");
-    (res = a % 5 === 0) ?
-        document.write("<p>" + a + " % 5 is not complete </p>") :
-        document.write("<p>" + a + " % 5 = " + res + "</p>");
-    (res = a % 6 === 0) ?
-        document.write("<p>" + a + " % 6 is not complete </p>") :
-        document.write("<p>" + a + " % 6 = " + res + "</p>");
-    (res = a % 9 === 0) ?
-        document.write("<p>" + a + " % 9 is not complete </p>") :
-        document.write("<p>" + a + " % 9 = " + res + "</p>");
+    (a % 2 !== 0) ?
+        document.write("<p>" + a + " is not divide on 2</p>") :
+        document.write("<p>" + a + " % 2 = " + a / 2 + "</p>");
+    (a % 3 !== 0) ?
+        document.write("<p>" + a + " is not divide on 3</p>") :
+        document.write("<p>" + a + " % 3 = " + a / 3 + "</p>");
+    (a % 5 !== 0) ?
+        document.write("<p>" + a + " is not divide on 5</p>") :
+        document.write("<p>" + a + " % 5 = " + a / 5 + "</p>");
+    (a % 6 !== 0) ?
+        document.write("<p>" + a + " is not divide on 6</p>") :
+        document.write("<p>" + a + " % 6 = " + a / 6 + "</p>");
+    (a % 9 !== 0) ?
+        document.write("<p>" + a + " is not divide on 9</p>") :
+        document.write("<p>" + a + " % 9 = " + a / 9 + "</p>");
 }
+someNumber(Math.round(Math.random() * 100));
+
+// optional task
+var menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function multiplyNumeric(obj) {
+    for (var key in obj) {
+        if(isNumeric(obj[key])) {
+            obj[key] = obj[key] * 2;
+        }
+    }
+}
+multiplyNumeric(menu);
+
+console.log(menu);
+
+let a = 10;
+
+let b = 20;
+
