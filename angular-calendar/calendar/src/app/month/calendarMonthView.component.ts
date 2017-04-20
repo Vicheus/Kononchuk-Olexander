d60c7d31ef8df012f5a4bf90b0740ea4e3bd9593
Olexandr Kononchuk <okononchuk@grossum.com>
2017-04-20 18:42:11 +0300

    angular calendar: added next/previous/today methods

--- angular-calendar/calendar/src/app/month/calendarMonthView.component.ts
+++ angular-calendar/calendar/src/app/month/calendarMonthView.component.ts
@@ -15,18 +15,36 @@ export class MonthViewComponent implements OnInit {
 
     previousMonthDateArray: Date[] = [];
 
+    nextMonthDateArray: Date[] = [];
+
+    temp;
+
     getDaysFromPreviousMonth() {
         let startDayCurrentMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1).getDay();
         let previousMonthCountDays = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 0).getDate();
-        let diffDays = startDayCurrentMonth - 1;
+        let diffDays;
+
+        (startDayCurrentMonth === 1) ?
+            diffDays = 0 :
+            (startDayCurrentMonth === 0) ?
+                diffDays = 1 :
+                diffDays = 8 - startDayCurrentMonth;
         let startDayPreviousMonth = previousMonthCountDays - 6 + diffDays;
 
         for (let i = startDayPreviousMonth; i <= previousMonthCountDays; i++) {
             let day = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1, i);
             this.previousMonthDateArray.push(day);
         }
+    }
 
-        return previousMonthCountDays;
+    getDaysFromNextMonth() {
+        let endDayCurrentMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 0).getDay();
+        let endDayNextMonth = 7 - endDayCurrentMonth;
+
+        for (let i = 1; i <= endDayNextMonth; i++) {
+            let day = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, i);
+            this.nextMonthDateArray.push(day);
+        }
     }
 
     getDateArray(): void {
@@ -37,12 +55,49 @@ export class MonthViewComponent implements OnInit {
         }
     }
 
+    nextMonthButton() {
+        this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 1);
+
+        this.dateArray = [];
+        this.previousMonthDateArray = [];
+        this.nextMonthDateArray = [];
+
+        this.getDateArray();
+        this.getDaysFromPreviousMonth();
+        this.getDaysFromNextMonth();
+    }
+
+    previousMonthButton() {
+        this.currentDate = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1, 1);
+
+        this.dateArray = [];
+        this.previousMonthDateArray = [];
+        this.nextMonthDateArray = [];
+
+        this.getDateArray();
+        this.getDaysFromPreviousMonth();
+        this.getDaysFromNextMonth();
+    }
+
+    todayButton() {
+        this.currentDate = new Date();
+
+        this.dateArray = [];
+        this.previousMonthDateArray = [];
+        this.nextMonthDateArray = [];
+
+        this.getDateArray();
+        this.getDaysFromPreviousMonth();
+        this.getDaysFromNextMonth();
+    }
+
     constructor() {
     }
 
     ngOnInit() {
         this.getDateArray();
         this.getDaysFromPreviousMonth();
+        this.getDaysFromNextMonth();
     }
 
 }
